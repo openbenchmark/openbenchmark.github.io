4954c5315a8dee57593ce395bce6a8a1c8e1fe41
xpai <xpai@users.noreply.github.com>
2023-02-09 17:17:21 +0800

    Update links

--- BarsCTR/js/nav.js
+++ BarsCTR/js/nav.js
@@ -9,10 +9,10 @@ function add_nav_li(link, link_name, page_name, mode = 0) {
     }
     a.innerHTML = link_name;
     if (mode == 1) {
-        a.setAttribute("target",'_blank');
+        a.setAttribute("target", '_blank');
     }
     if (page_name==link_name) {
-        new_nav.setAttribute("class","active");
+        new_nav.setAttribute("class", "active");
     }
     new_nav.appendChild(a);
     document.getElementById("nav_ul").appendChild(new_nav);
@@ -23,12 +23,12 @@ function add_nav_ul(name, data, page_name) {
     a0.href = "#LeadBoard";
     a0.setAttribute("data-toggle", "collapse");
     a0.setAttribute("aria-expanded", false);
-    a0.setAttribute("class","dropdown-toggle");
+    a0.setAttribute("class", "dropdown-toggle");
     a0.innerHTML = name;
     new_li.appendChild(a0);
     var new_ul = document.createElement('ul');
-    new_ul.setAttribute("class","collapse list-unstyled");
-    new_ul.setAttribute("id","LeadBoard");
+    new_ul.setAttribute("class", "collapse list-unstyled");
+    new_ul.setAttribute("id", "LeadBoard");
 
     for (var i = 0; i < data.length; i = i + 1) {
         var sub_li = document.createElement('li');
@@ -54,7 +54,7 @@ function add_nav_ul(name, data, page_name) {
 
 function add_nav(page_name) {
     // load nav json data 
-    var nav_json = "https://openbenchmark.github.io/candidate-matching/js/nav.json"
+    var nav_json = "https://openbenchmark.github.io/BarsCTR/js/nav.json"
     var request = new XMLHttpRequest();
     request.open("get", nav_json);
     request.send(null);
@@ -63,14 +63,14 @@ function add_nav(page_name) {
             var nav_data = JSON.parse(request.responseText);
             for (var i = 0; i < nav_data.length; i = i + 1) {
                 if (nav_data[i].hasOwnProperty("type")) {
-                    add_nav_ul(name=nav_data[i]["name"],data=nav_data[i]["data"],page_name=page_name);
+                    add_nav_ul(name=nav_data[i]["name"], data=nav_data[i]["data"], page_name=page_name);
                 }
                 else {
                     if (nav_data[i].hasOwnProperty("name")) {
-                        add_nav_li(link = nav_data[i]['link'], link_name = nav_data[i]["name"], page_name,mode = nav_data[i]["mode"]);
+                        add_nav_li(link=nav_data[i]['link'], link_name=nav_data[i]["name"], page_name, mode=nav_data[i]["mode"]);
                     }
                     else {
-                        add_nav_li(link = nav_data[i]["link"], link = nav_data[i]["name"],page_name);
+                        add_nav_li(link=nav_data[i]["link"], link=nav_data[i]["name"],page_name);
                     }
                 }
 
